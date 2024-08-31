cebf0b4b02b410b6700a320a994e9583f710e448
hzj <huangzhijun@sutpc.com>
2024-08-31 16:44:27 +0800

    define IG_LIB_API

--- IGFileDialog.h
+++ IGFileDialog.h
@@ -1,7 +1,13 @@
 #ifndef __IMGUI_EXT_FILE_DIALOG
 #define __IMGUI_EXT_FILE_DIALOG
 
-extern "C" __declspec(dllexport) const char* IGGetFile();
-extern "C" __declspec(dllexport) const char* IGGetDirectory();
+#ifdef FILEDLG_LIB_IMPORT
+#define IG_LIB_API __declspec(dllimport)
+#else
+#define IG_LIB_API __declspec(dllexport)
+#endif
+
+extern "C" IG_LIB_API const char* IGGetFile();
+extern "C" IG_LIB_API const char* IGGetDirectory();
 
 #endif // !__IMGUI_FILE_DIALOG
